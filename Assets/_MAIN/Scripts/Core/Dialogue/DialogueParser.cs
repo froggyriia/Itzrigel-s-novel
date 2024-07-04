bf803f0acf66d82117851d763bd188118584c39a
froggyriia <nira.osamu@yandex.ru>
2024-07-04 23:16:01 +0700

    continue working on parsing system
    ep 03 pt 2 completed

--- Assets/_MAIN/Scripts/Core/Dialogue/DialogueParser.cs
+++ Assets/_MAIN/Scripts/Core/Dialogue/DialogueParser.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 namespace DIALOGUE
 {
     public class DialogueParser
     {
+        private const string commandregexPattern = "\\w*[^\\s]\\(";
         public static DIALOGUE_LINE Parse(string rawLine)
         {
 
             Debug.Log($"Parsing line - '{rawLine}'");
 
             (string speaker, string dialogue, string commands) = RipContent(rawLine);
+
+            Debug.Log($"speaker {speaker}\ndialogue: {dialogue}\ncommands: {commands}");
 
             return new DIALOGUE_LINE(speaker, dialogue, commands);
         }
@@ -29,6 +33,7 @@ namespace DIALOGUE
                 char current = rawline[i];
                 if (current == '\\')
                     isEscaped = !isEscaped;
+
                 else if (current == '"' && !isEscaped)
                 {
                     if (dialogueStart == -1)
@@ -38,17 +43,35 @@ namespace DIALOGUE
                     else if (dialogueEnd == -1)
                     {
                         dialogueEnd = i;
-                        //break;
+                        break;
                     }
-                    else
-                        isEscaped = false;
                 }
+                else
+                    isEscaped = false;
 
-                
-                
             }
-            Debug.Log($"dialogue start: {dialogueStart}, dialogueEnd: {dialogueEnd}");
-            Debug.Log(rawline.Substring(dialogueStart + 1, dialogueEnd - dialogueStart - 1));
+            //Identify Command Pattern
+            Regex commandRegex = new Regex(commandregexPattern);
+            Match match = commandRegex.Match(rawline);
+            int commandStart = -1;
+            if (match.Success)
+            {
+                commandStart = match.Index;
+                if (dialogueStart == -1 && dialogueEnd == -1)
+                    return ("", "", rawline.Trim());
+            }
+
+            if (dialogueStart != -1 && dialogueEnd != -1 && (commandStart == -1 || commandStart > dialogueStart))
+            {
+                speaker = rawline.Substring(0, dialogueStart).Trim();
+                dialogue = rawline.Substring(dialogueStart + 1, dialogueEnd - dialogueStart - 1).Replace("\\\"", "\"");
+                if (commandStart != -1)
+                    commands = rawline.Substring(commandStart).Trim();
+            }
+            else if (commandStart != -1 && dialogueStart > commandStart)
+                commands = rawline;
+            else
+                speaker = rawline;
             return (speaker, dialogue, commands);
         }
     }
--- Assets/_TESTING/Scripts/TestParsing.cs
+++ Assets/_TESTING/Scripts/TestParsing.cs
@@ -7,17 +7,21 @@ namespace TESTING
 {
     public class TestParsing : MonoBehaviour
     {
+        [SerializeField] private TextAsset file;
         // Start is called before the first frame update
         void Start()
         {
-            string line = "Speaker \" Dialogue passes you...\"";
-
-            DialogueParser.Parse(line);
+            SendFileToParse();
         }
 
-        // Update is called once per frame
-        void Update()
+        void SendFileToParse()
         {
+            List<string> lines = FileManager.ReadTextAsset("testFile", false);
+
+            foreach(string line in lines)
+            {
+                DIALOGUE_LINE dl = DialogueParser.Parse(line);
+            }
 
         }
     }
